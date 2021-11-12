d5af20a1ab8d881dee79dcb7fac6968b96f31bf4
shawn-munir <shawnmunir89@gmail.com>
2021-11-12 10:42:14 -0600

    Create pandas profiler.py

---
+++ pandas profiler.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+
+df = pd.DataFrame(np.random.rand(100, 5), columns=["a", "b", "c", "d", "e"])
+
+profile = ProfileReport(df, title="Pandas Profiling Report")
