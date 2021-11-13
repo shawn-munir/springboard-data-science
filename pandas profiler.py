b6fc334ea66c9ea6f1fa2755bc3c7b6f54571ec1
shawn-munir <shawnmunir89@gmail.com>
2021-11-12 18:54:13 -0600

    Update pandas profiler.py

--- pandas profiler.py
+++ pandas profiler.py
@@ -6,3 +6,15 @@ from pandas_profiling import ProfileReport
 df = pd.DataFrame(np.random.rand(100, 5), columns=["a", "b", "c", "d", "e"])
 
 profile = ProfileReport(df, title="Pandas Profiling Report")
+
+profile2 = ProfileReport(df, title="Pandas Profiling Report", explorative=True)
+
+profile.to_file("your_report.html")
+
+#OR
+
+# As a string
+json_data = profile.to_json()
+
+# As a file
+profile.to_file("your_report.json")
